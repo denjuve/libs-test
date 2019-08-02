13a8a580a09cc1ed1ab9c0e67d658a5e227c52a9
Denis Kucherenko <den.ku4er@gmail.com>
2019-08-02 15:46:07 +0300

    checking docker installation libruary ext1 (remote println+1remote)

--- src/com/libs/dockerCheckRemote.groovy
+++ src/com/libs/dockerCheckRemote.groovy
@@ -11,15 +11,18 @@ remote.allowAnyHosts = true
 node {
 //  withCredentials([sshUserPrivateKey(credentialsId: "${params.ssh_creds}", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
 //  withCredentials([sshUserPrivateKey(credentialsId: params.ssh_creds, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
-  withCredentials([sshUserPrivateKey(credentialsId: "$ssh_creds1", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
-])
+//  withCredentials([sshUserPrivateKey(credentialsId: "$ssh_creds1", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
+//])
 //      usernamePassword(credentialsId: '5gt-ci', usernameVariable: 'u5g', passwordVariable: 'p5g')]) {
-        remote.user = userName
-        remote.identityFile = identity
+//        remote.user = userName
+//        remote.identityFile = identity
 println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!${remote.name}"
+println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!${remote.host}"
+println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!${remote.allowAnyHosts}"
 println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!$ssh_creds1"
+println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!$params.ssh_creds"
 //
-    sshCommand remote: remote, command: "rm -rf /tmp/scripts-my"
+//    sshCommand remote: remote, command: "rm -rf /tmp/scripts-my"
 //    sshCommand remote: remote, command: "if sudo docker version; then echo 'Docker installed'; else rm -rf /tmp/scripts-my; git clone https://github.com/denjuve/scripts.git /tmp/scripts-my; bash /tmp/scripts-my/docker_install.sh; fi"
 //    sshCommand remote: remote, command: "if sudo docker-compose --version; then echo 'Compose installed'; else sudo curl -L https://github.com/docker/compose/releases/download/1.23.0-rc3/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose; sudo chmod +x /usr/local/bin/docker-compose; fi"
 //    sshCommand remote: remote, command: "git config --global http.sslVerify false"
