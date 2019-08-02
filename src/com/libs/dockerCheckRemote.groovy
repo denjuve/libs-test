70191e6af5d0fdd974691d6e58470780fdf7fe74
Denis Kucherenko <den.ku4er@gmail.com>
2019-08-02 13:07:16 +0300

    checking docker installation libruary ext1 (remote println)

--- src/com/libs/dockerCheckRemote.groovy
+++ src/com/libs/dockerCheckRemote.groovy
@@ -8,16 +8,16 @@ def remote = [:]
 remote.name = params.rhost_ip_mon
 remote.host = params.rhost_ip_mon
 remote.allowAnyHosts = true
-println ${remote.name}
 node {
 //  withCredentials([sshUserPrivateKey(credentialsId: "${params.ssh_creds}", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
 //  withCredentials([sshUserPrivateKey(credentialsId: params.ssh_creds, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
-  withCredentials([sshUserPrivateKey(credentialsId: "$ssh_creds1", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
-])
+//  withCredentials([sshUserPrivateKey(credentialsId: "$ssh_creds1", keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
+////])
 //      usernamePassword(credentialsId: '5gt-ci', usernameVariable: 'u5g', passwordVariable: 'p5g')]) {
 //        remote.user = userName
 //        remote.identityFile = identity
-println $ssh_creds1
+println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!${remote.name}"
+println "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!$ssh_creds1"
 //
 //    sshCommand remote: remote, command: "rm -rf /tmp/scripts-my"
 //    sshCommand remote: remote, command: "if sudo docker version; then echo 'Docker installed'; else rm -rf /tmp/scripts-my; git clone https://github.com/denjuve/scripts.git /tmp/scripts-my; bash /tmp/scripts-my/docker_install.sh; fi"
