7ad0261af835362f3bd8a4eff029c1b5ec4df28b
Denis Kucherenko <den.ku4er@gmail.com>
2019-08-21 18:08:23 +0300

    fnctn with args deploy lib (fix few args) + verify lib

--- src/com/libs/buildDeploRemote.groovy
+++ src/com/libs/buildDeploRemote.groovy
@@ -1,12 +1,13 @@
 package com.libs
 
-def remoteDeploy($d_dir, $s_dir, component_name, ci_branch_repo, ssh_creds, remote_ip) {
+def remoteDeploy(d_dirp, s_dirp, component_name, ci_branch_repo, git_branch_repo, ssh_creds, remote_ip) {
 
-string d_dir = $d_dir
-string s_dir = $s_dir
+string d_dir = d_dirp
+string s_dir = s_dirp
 string cmpt_id = component_name
 string ci_rep = ci_branch_repo
-string git_mon = params.git_branch_mon
+//string git_mon = params.git_branch_mon
+string git_rep = git_branch_repo
 
 def remote = [:]
 remote.name = remote_ip
@@ -18,12 +19,6 @@ remote.allowAnyHosts = true
         remote.user = userName
         remote.identityFile = identity
 
-////arguments for the function//****************************
-//remote_ip (scp)
-//ci_creds [$u5g:$p5g] (creds git_ci) ????????????????????????? !!!!!!withcredentials!!!
-//${identity} (scp) !!!!!!withcredentials!!!
-//${userName} (scp) !!!!!!withcredentials!!!
-
     sh 'rm -rf repo'
     sshCommand remote: remote, command: "rm -rf $d_dir || true"
     sshCommand remote: remote, command: "sudo docker rm -f \$(sudo docker ps | grep $cmpt_id | awk '{ print \$1}')  || true"
@@ -33,9 +28,8 @@ remote.allowAnyHosts = true
     sh "scp -r -i ${identity} -o StrictHostKeyChecking=no $s_dir/* ${userName}@${remote_ip}:$d_dir"
 
     sshCommand remote: remote, command: "sed -i 's/5g-transformer.eu/$u5g:$p5g@5g-transformer.eu/g' $d_dir/${cmpt_id}build_docker.sh"
-    sshCommand remote: remote, command: "sed -i 's/GIT_BRANCH=.*/GIT_BRANCH=$git_mon/' $d_dir/${cmpt_id}build_docker.sh"
+    sshCommand remote: remote, command: "sed -i 's/GIT_BRANCH=.*/GIT_BRANCH=$git_rep/' $d_dir/${cmpt_id}build_docker.sh"
     sshCommand remote: remote, command: "chmod +x $d_dir/${cmpt_id}build_docker.sh"
-    sshCommand remote: remote, command: "sed -i 's/#sudo/sudo/g' $d_dir/${cmpt_id}build_docker.sh"
 
     sshCommand remote: remote, command: "bash $d_dir/${cmpt_id}build_docker.sh"
 
---
+++ src/com/libs/verify.groovy
@@ -0,0 +1,48 @@
+package com.libs
+
+//def remoteDeploy($d_dir, $s_dir, component_name, ci_branch_repo, ssh_creds, remote_ip) {
+def remoteDeploy(d_dirp, s_dirp, component_name, ssh_creds, remote_ip) {
+
+string d_dir = d_dirp
+string s_dir = s_dirp
+string cmpt_id = component_name
+//string ci_rep = ci_branch_repo
+
+def remote = [:]
+remote.name = remote_ip
+remote.host = remote_ip
+remote.allowAnyHosts = true
+
+  withCredentials([sshUserPrivateKey(credentialsId: ssh_creds, keyFileVariable: 'identity', passphraseVariable: '', usernameVariable: 'userName'),
+      usernamePassword(credentialsId: '5gt-ci', usernameVariable: 'u5g', passwordVariable: 'p5g')]) {
+        remote.user = userName
+        remote.identityFile = identity
+
+    sshCommand remote: remote, command: "sudo locale-gen de_DE.utf8 en_US.utf8 es_ES.utf8 it_IT.utf8 ru_RU.utf8 uk_UA.utf8"
+    sshCommand remote: remote, command: "sudo locale -a"
+    sshCommand remote: remote, command: 'sudo apt update -y'
+    sshCommand remote: remote, command: "sudo apt install -y python-pip virtualenv"
+    
+    sshCommand remote: remote, command: "mkdir -p -m 0777 ~/virtualenvironment/${cmpt_id}_test || true"
+    sshCommand remote: remote, command: "virtualenv ~/virtualenvironment/${cmpt_id}_test"
+    sshCommand remote: remote, command: "cp -r $d_dir/test/test_port.py ~/virtualenvironment/vs_test/bin/"
+
+    sh '''
+cat > /tmp/${cmpt_id}_test.sh << EOF
+#!/bin/bash
+cd ~/virtualenvironment/${cmpt_id}test/bin/          
+source activate
+pip install pytest
+pytest --junitxml=${cmpt_id}_report_port.xml -x -v test_port.py || true
+deactivate
+EOF
+    '''
+    sshPut remote: remote, from: "/tmp/${cmpt_id}_test.sh", into: "/tmp/${cmpt_id}_test.sh", override: true
+    sshCommand remote: remote, command: "chmod +x /tmp/${cmpt_id}_test.sh"
+    sshCommand remote: remote, command: "bash /tmp/${cmpt_id}_test.sh"
+    sshGet remote: remote, from: "$d_dir/test/${cmpt_id}_report_port.xml", into: "$s_dir/${cmpt_id}_report_port.xml", override: true
+    junit testResults: "$s_dir/${cmpt_id}_report_port.xml"
+    
+    sshCommand remote: remote, command: "rm -rf /tmp/*${cmpt_id}*"
+    sh "rm -rf /tmp/*${cmpt_id}*"
+}}
