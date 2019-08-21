5b8788a044d97ea1dfabebe1fbedf849e8f5eaa0
Denis Kucherenko <den.ku4er@gmail.com>
2019-08-21 12:17:18 +0300

    test fnctn with args deploy lib (arg doesn't resolve-d/s_path-add 2 more vars)

--- src/com/libs/buildDeploRemote.groovy
+++ src/com/libs/buildDeploRemote.groovy
@@ -1,7 +1,9 @@
 package com.libs
 
-def remoteDeploy(dest_dir, source_dir, component_name, ci_branch_repo, ssh_creds, remote_ip) {
+def remoteDeploy($d_dir, $s_dir, component_name, ci_branch_repo, ssh_creds, remote_ip) {
 
+string d_dir = $d_dir
+string s_dir = $s_dir
 string cmpt_id = component_name
 string ci_rep = ci_branch_repo
 
@@ -16,8 +18,8 @@ remote.allowAnyHosts = true
         remote.identityFile = identity
 
 ////arguments for the function//****************************
-//source_dir [--> $s_path_mon/s_path_mtp etc.]
-//dest_dir [--> $d_path_mon/d_path_mtp etc.]
+//$s_dir [--> $s_path_mon/s_path_mtp etc.]
+//$d_dir [--> $d_path_mon/d_path_mtp etc.]
 //grep mon_ (component containers deletion) ????????  component_name [--> mon_]
 //ci_branch_repo [-->${params.ci_branch_mon}]
 //ssh_id [-->${params.ssh_creds}]
@@ -28,19 +30,19 @@ remote.allowAnyHosts = true
 //${userName} (scp) !!!!!!withcredentials!!!
 
     sh 'rm -rf repo'
-    sshCommand remote: remote, command: "rm -rf dest_dir || true"
+    sshCommand remote: remote, command: "rm -rf $d_dir || true"
     sshCommand remote: remote, command: "sudo docker rm -f \$(sudo docker ps | grep $cmpt_id | awk '{ print \$1}')  || true"
-    sshCommand remote: remote, command: "mkdir -p -m 0777 dest_dir"
+    sshCommand remote: remote, command: "mkdir -p -m 0777 $d_dir"
 
     sh "git clone -b $ci_rep https://$u5g:$p5g@5g-transformer.eu/git/5g-transformer.5gt-ci repo"
-    sh "scp -r -i ${identity} -o StrictHostKeyChecking=no $s_path/* ${userName}@${remote_ip}:dest_dir"
+    sh "scp -r -i ${identity} -o StrictHostKeyChecking=no $s_dir/* ${userName}@${remote_ip}:$d_dir"
 
-    sshCommand remote: remote, command: "sed -i 's/5g-transformer.eu/$u5g:$p5g@5g-transformer.eu/g' dest_dir/$cmpt_idbuild_docker.sh"
-    sshCommand remote: remote, command: "sed -i 's/GIT_BRANCH=.*/GIT_BRANCH=params.git_branch_mon/' dest_dir/$cmpt_idbuild_docker.sh"
-    sshCommand remote: remote, command: "chmod +x dest_dir/$cmpt_idbuild_docker.sh"
-    sshCommand remote: remote, command: "sed -i 's/#sudo/sudo/g' dest_dir/$cmpt_idbuild_docker.sh"
+    sshCommand remote: remote, command: "sed -i 's/5g-transformer.eu/$u5g:$p5g@5g-transformer.eu/g' $d_dir/$cmpt_idbuild_docker.sh"
+    sshCommand remote: remote, command: "sed -i 's/GIT_BRANCH=.*/GIT_BRANCH=params.git_branch_mon/' $d_dir/$cmpt_idbuild_docker.sh"
+    sshCommand remote: remote, command: "chmod +x $d_dir/$cmpt_idbuild_docker.sh"
+    sshCommand remote: remote, command: "sed -i 's/#sudo/sudo/g' $d_dir/$cmpt_idbuild_docker.sh"
 
-    sshCommand remote: remote, command: "bash dest_dir/$cmpt_idbuild_docker.sh"
+    sshCommand remote: remote, command: "bash $d_dir/$cmpt_idbuild_docker.sh"
 
     sshCommand remote: remote, command: "sudo docker ps -a|grep $cmpt_id"
 }}
