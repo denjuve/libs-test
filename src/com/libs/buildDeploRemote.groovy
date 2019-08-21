1607ec8aa911b6826767c8a8bb04f7509ae9cb56
Denis Kucherenko <den.ku4er@gmail.com>
2019-08-21 12:04:13 +0300

    test fnctn with args deploy lib (2 args doesn't resolve - mon_ & ci_branch_repo-enable var)

--- src/com/libs/buildDeploRemote.groovy
+++ src/com/libs/buildDeploRemote.groovy
@@ -2,6 +2,8 @@ package com.libs
 
 def remoteDeploy(dest_dir, source_dir, component_name, ci_branch_repo, ssh_creds, remote_ip) {
 
+string cmpt_id = component_name
+
 def remote = [:]
 remote.name = remote_ip
 remote.host = remote_ip
@@ -12,10 +14,7 @@ remote.allowAnyHosts = true
         remote.user = userName
         remote.identityFile = identity
 
-//string cmpt_id = component_name
-
-////arguments for the function
-//****************************
+////arguments for the function//****************************
 //source_dir [--> $s_path_mon/s_path_mtp etc.]
 //dest_dir [--> $d_path_mon/d_path_mtp etc.]
 //grep mon_ (component containers deletion) ????????  component_name [--> mon_]
@@ -23,14 +22,13 @@ remote.allowAnyHosts = true
 //ssh_id [-->${params.ssh_creds}]
 
 //remote_ip (scp)
-
 //ci_creds [$u5g:$p5g] (creds git_ci) ????????????????????????? !!!!!!withcredentials!!!
 //${identity} (scp) !!!!!!withcredentials!!!
 //${userName} (scp) !!!!!!withcredentials!!!
 
     sh 'rm -rf repo'
     sshCommand remote: remote, command: "rm -rf dest_dir || true"
-    sshCommand remote: remote, command: "sudo docker rm -f \$(sudo docker ps | grep component_name | awk '{ print \$1}')  || true"
+    sshCommand remote: remote, command: "sudo docker rm -f \$(sudo docker ps | grep $cmpt_id | awk '{ print \$1}')  || true"
     sshCommand remote: remote, command: "mkdir -p -m 0777 dest_dir"
 
     sh "git clone -b ci_branch_repo https://$u5g:$p5g@5g-transformer.eu/git/5g-transformer.5gt-ci repo"
@@ -45,4 +43,4 @@ remote.allowAnyHosts = true
 
     sshCommand remote: remote, command: "sudo docker ps -a|grep $cmpt_id"
 }}
-
+return this
