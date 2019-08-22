c574a06364007fee8dbc82e65cddf56b2fab13e5
Denis Kucherenko <den.ku4er@gmail.com>
2019-08-22 12:08:34 +0300

    verify lib fnctn bash script creation issues4

--- src/com/libs/verify.groovy
+++ src/com/libs/verify.groovy
@@ -31,8 +31,11 @@ remote.allowAnyHosts = true
     sshCommand remote: remote, command: "echo 'source activate' >> /tmp/${cmpt_id}_test.sh"
     sshCommand remote: remote, command: "echo 'pip install pytest' >> /tmp/${cmpt_id}_test.sh"
 //    sshCommand remote: remote, command: "echo 'pytest --junitxml=${cmpt_id}_report_port.xml -x -v test_port.py || true' >> /tmp/${cmpt_id}_test.sh"
-    sshCommand remote: remote, command: "echo $echo_line >> /tmp/${cmpt_id}_test.sh"
+//sed '2 s/.*/new_text_here/' filename
+    sshCommand remote: remote, command: "echo problem >> /tmp/${cmpt_id}_test.sh"
+    sshCommand remote: remote, command: "sed -i '5 s/.*/pytest --junitxml=${cmpt_id}_report_port.xml -x -v test_port.py || true@/tmp/${cmpt_id}_test.sh"
     sshCommand remote: remote, command: "echo deactivate >> /tmp/${cmpt_id}_test.sh"
+    sshCommand remote: remote, command: "echo $echo_line >> /tmp/${cmpt_id}_test.sh"
 
 //    sh '''
 //cat > /tmp/${cmpt_id}_test.sh << EOF
@@ -44,7 +47,8 @@ remote.allowAnyHosts = true
 //deactivate
 //EOF
 //    '''
-    sshPut remote: remote, from: "/tmp/${cmpt_id}_test.sh", into: "/tmp/${cmpt_id}_test.sh", override: true
+
+//    sshPut remote: remote, from: "/tmp/${cmpt_id}_test.sh", into: "/tmp/${cmpt_id}_test.sh", override: true
     sshCommand remote: remote, command: "chmod +x /tmp/${cmpt_id}_test.sh"
     sshCommand remote: remote, command: "bash /tmp/${cmpt_id}_test.sh"
     sshGet remote: remote, from: "$d_dir/test/${cmpt_id}_report_port.xml", into: "$s_dir/${cmpt_id}_report_port.xml", override: true
